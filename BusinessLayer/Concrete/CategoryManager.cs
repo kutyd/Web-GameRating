bfe9c00e368c70f9e04eaca084bfd9c5b08ff899
yusufysn <104219425+yusufysn@users.noreply.github.com>
2022-12-26 22:34:27 +0300

    Add files via upload

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBl(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length<=3 ||
+                p.CategoryDescrption == "" || p.CategoryName.Length>=51)
+            {
+                //hata mesaji
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20221225173055_mig1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    AboutID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AboutDetails1 = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    AboutDetails2 = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    AboutImage1 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AboutImage2 = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.AboutID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "integer", maxLength: 1000, nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CategoryDescrption = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CategoryStatus = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    UserMail = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Subject = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    WriterID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WriterName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    WriterSurname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    WriterImage = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    WriterMail = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    WriterPasswort = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.WriterID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Headings",
+                columns: table => new
+                {
+                    HeadingID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HeadingName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    HeadingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CategoryID = table.Column<int>(type: "integer", nullable: false),
+                    WriterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Headings", x => x.HeadingID);
+                    table.ForeignKey(
+                        name: "FK_Headings_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Headings_Writers_WriterId",
+                        column: x => x.WriterId,
+                        principalTable: "Writers",
+                        principalColumn: "WriterID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    ContentID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ContentValue = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    ContentDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HeadingId = table.Column<int>(type: "integer", nullable: true),
+                    WriterId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.ContentID);
+                    table.ForeignKey(
+                        name: "FK_Contents_Headings_HeadingId",
+                        column: x => x.HeadingId,
+                        principalTable: "Headings",
+                        principalColumn: "HeadingID");
+                    table.ForeignKey(
+                        name: "FK_Contents_Writers_WriterId",
+                        column: x => x.WriterId,
+                        principalTable: "Writers",
+                        principalColumn: "WriterID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_HeadingId",
+                table: "Contents",
+                column: "HeadingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_WriterId",
+                table: "Contents",
+                column: "WriterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Headings_CategoryID",
+                table: "Headings",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Headings_WriterId",
+                table: "Headings",
+                column: "WriterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "Headings");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
